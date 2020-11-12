0701de57b212f26a3a8480d16937ce2462357c28
Liaojinyu <1976902758@qq.com>
2020-11-12 18:40:08 +0800

    完成购物车的样式

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -3,10 +3,42 @@
 		<view class="clear-invalid width-90">
 			清空失效商品
 		</view>
-		<view class="cart-item flex">
+		<view class="cart-item width-90 flex">
 			<u-checkbox @change="checkboxChange" size="30" shape="circle" active-color="#fa5376" class="select-dot"></u-checkbox>
 			<view class="flex justify-between" style="width: 100%;">
-				<image src="../../static/images/face.jpg" mode=""></image>
+				<view class="image">
+					<image src="../../static/images/face.jpg" mode=""></image>
+					<view class="invalid-mask">
+						已失效
+					</view>
+				</view>
+				<view class="cart-item-right flex flex-direction">
+					<view class="title">
+						DUNKINDONUTS唐恩都乐美国甜甜圈6个礼盒装
+					</view>
+					<view class="attribute">
+						<text>草莓味【8枚】</text>
+					</view>
+					<view class="flex justify-between">
+						<text class="price">￥35.90</text>
+						<view class="group flex">
+							<icon class="iconfont icon-jian2"></icon>
+							<input disabled="true" class="input" type="text" value="1" />
+							<icon class="iconfont icon-jia"></icon>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="cart-item width-90 flex">
+			<u-checkbox @change="checkboxChange" size="30" shape="circle" active-color="#fa5376" class="select-dot"></u-checkbox>
+			<view class="flex justify-between" style="width: 100%;">
+				<view class="image">
+					<image src="../../static/images/face.jpg" mode=""></image>
+					<view class="invalid-mask">
+						已失效
+					</view>
+				</view>
 				<view class="cart-item-right flex flex-direction">
 					<view class="title">
 						DUNKINDONUTS唐恩都乐美国甜甜圈6个礼盒装
@@ -26,14 +58,15 @@
 			</view>
 		</view>
 		<view class="cart-bottom">
-			<view class="flex justify-between">
+			<view class="flex justify-between width-90">
 				<view class="cart-bottom-left">
 					<u-checkbox @change="checkboxChange" size="30" shape="circle" active-color="#fa5376" class="select-dot"></u-checkbox>
 					全选
 				</view>
 				<view class="cart-bottom-right flex justify-between">
-					<text>合计：￥</text>
-					<button type="default">结算</button>
+					<text>合计：￥0</text>
+					<u-button class="custom-style" shape="circle" :hair-line="false" size="mini">结算(0)</u-button>
+					<!-- <button type="circle">结算</button> -->
 				</view>
 			</view>
 		</view>
@@ -67,16 +100,35 @@
 			box-shadow: #eee 0 2rpx 6rpx 5rpx;
 			border-radius: 8rpx;
 			height: 200rpx;
+			margin-bottom: 20rpx;
 			.select-dot{
-				margin-left: 3rpx;
+				margin-left: 20rpx;
 			}
-			image{
+			.image{
 				width: 165rpx;
 				height: 165rpx;
-				padding: 10rpx;
+				margin: 10rpx;
+				position: relative;
+				image{
+					width: 100%;
+					height: 100%;
+					
+				}
+				.invalid-mask{
+					position: absolute;
+					top: 0;
+					left: 0;
+					width: 100%;
+					height: 100%;
+					line-height: 165rpx;
+					color: #fff;
+					font-size: 24rpx;
+					text-align: center;
+					background: rgba(0,0,0,0.5);
+				}
 			}
 			.cart-item-right{
-				width: 490rpx;
+				width: 460rpx;
 				box-sizing: border-box;
 				padding: 10rpx;
 				.title{
@@ -124,10 +176,14 @@
 			.cart-bottom-left{
 				width: 30%;
 			}
-			// background-color: pink;
 			.cart-bottom-right{
-				width: 40%;
-				background-color: red;
+				width: 50%;
+				align-items: center;
+				.custom-style {
+						display: inline-block;
+						color: #fff;
+						background: #fa436a;
+					}
 			}
 		}
 	}
